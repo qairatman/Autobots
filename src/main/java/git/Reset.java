bf87c25497b7b5452b933df3a6f603d30b86ec7a
Kairat <qairatman@gmail.com>
2025-07-08 19:55:16 +0600

    4

--- src/main/java/git/Reset.java
+++ src/main/java/git/Reset.java
@@ -4,6 +4,7 @@ public class Reset {
     public static void main(String[] args) {
         System.out.println("kairat 1");
         System.out.println("kairat 2");
+        System.out.println("kairat 3");
 
     }
 }
